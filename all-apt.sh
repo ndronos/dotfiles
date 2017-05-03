5e14335387af83da32348864993709788953042c
Leandro Leite <ndronos@gmail.com>
2017-05-03 00:37:55 -0300

    [apt] Improve echo messages

--- all-apt.sh
+++ all-apt.sh
@@ -2,18 +2,18 @@
 clear
 echo "apt-get"
 
-echo "" && echo "Update *"
+echo "" && echo "Update" && echo " *"
 sudo apt-get update -y
-echo "" && echo "Upgrade *"
+echo "" && echo "Upgrade" && echo " *"
 sudo apt-get upgrade -y
-echo "" && echo "Dist-upgrade *"
+echo "" && echo "Dist-upgrade" && echo " *"
 sudo apt-get dist-upgrade -y
 
-echo "" && echo "Clean *"
+echo "" && echo "Clean" && echo " *"
 sudo apt-get clean -y
-echo "" && echo "Autoclean *"
+echo "" && echo "Autoclean" && echo " *"
 sudo apt-get autoclean -y
-echo "" && echo "Autoremove *"
+echo "" && echo "Autoremove" && echo " *"
 sudo apt-get autoremove -y
 
 echo ""
